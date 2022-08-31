a345675eb402cc0a418a5e4598b7c9f97e78669f
Cotofrec <cotofrecvictor@gmail.com>
2022-08-31 12:48:26 +0300

    create add task form

--- tody/acasa/urls.py
+++ tody/acasa/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import ListaSarcini
+from .views import CreazaSarcina, ListaSarcini
 
 urlpatterns = [
     path('', ListaSarcini.as_view(), name = 'sarcini'),
+    path('creaza-sarcina/', CreazaSarcina.as_view(), name = 'creaza-sarcina'),
 ]
--- tody/acasa/views.py
+++ tody/acasa/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Sarcini
+
 
 # Create your views here.
 
 class ListaSarcini(ListView):
     model= Sarcini
     context_object_name = 'sarcini'
+
+class CreazaSarcina(CreateView):
+    model = Sarcini
+    fields = '__all__'
+    success_url = reverse_lazy('sarcini')
