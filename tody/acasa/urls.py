006e5ce7d0d0e6953a3383668f58d4cacc05c39e
Cotofrec <cotofrecvictor@gmail.com>
2022-08-31 17:01:08 +0300

    implement logout

--- tody/acasa/urls.py
+++ tody/acasa/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
 from .views import CreazaSarcina, DetaliiSarcina, ListaSarcini,EditeazaSarcina, StergeSarcina, LogareView
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', LogareView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
     path('', ListaSarcini.as_view(), name = 'sarcini'),
     path('creaza-sarcina/', CreazaSarcina.as_view(), name = 'creaza-sarcina'),
     path('sarcina/<int:pk>/', DetaliiSarcina.as_view(), name = 'sarcina'),
