d5537da865585d9212a4323d2039400d566b7d53
Cotofrec <cotofrecvictor@gmail.com>
2022-08-31 19:27:55 +0300

    created user registration form with auto-login

--- tody/acasa/urls.py
+++ tody/acasa/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import CreazaSarcina, DetaliiSarcina, ListaSarcini,EditeazaSarcina, StergeSarcina, LogareView
+from .views import CreazaSarcina, DetaliiSarcina, ListaSarcini,EditeazaSarcina, StergeSarcina, LogareView, PaginaInregistrare
 from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', LogareView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
+    path('inregistrare/', PaginaInregistrare.as_view(), name ='inregistrare'),
     path('', ListaSarcini.as_view(), name = 'sarcini'),
     path('creaza-sarcina/', CreazaSarcina.as_view(), name = 'creaza-sarcina'),
     path('sarcina/<int:pk>/', DetaliiSarcina.as_view(), name = 'sarcina'),
--- tody/acasa/views.py
+++ tody/acasa/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView
 
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Sarcini
 
@@ -21,6 +23,18 @@ class LogareView(LoginView):
     def get_success_url(self):
         return reverse_lazy('sarcini')
 
+class PaginaInregistrare(FormView):
+    template_name = 'acasa/inregistrare.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('sarcini')
+
+    def form_valid(self,form):
+        utilizator = form.save()
+        if utilizator is not None:
+            login(self.request, utilizator)
+        return super(PaginaInregistrare, self).form_valid(form)
+
 class ListaSarcini(LoginRequiredMixin, ListView):
     model= Sarcini
     context_object_name = 'sarcini'
@@ -32,8 +46,12 @@ class ListaSarcini(LoginRequiredMixin, ListView):
 
 class CreazaSarcina(LoginRequiredMixin, CreateView):
     model = Sarcini
-    fields = '__all__'
+    fields = ['titlu','completat']
     success_url = reverse_lazy('sarcini')
+
+    def form_valid(self, form):
+        form.instance.utilizator = self.request.user
+        return super(CreazaSarcina, self).form_valid(form)
 
 class DetaliiSarcina(LoginRequiredMixin, DetailView):
     model = Sarcini
@@ -42,7 +60,7 @@ class DetaliiSarcina(LoginRequiredMixin, DetailView):
 
 class EditeazaSarcina(LoginRequiredMixin, UpdateView):
     model = Sarcini
-    fields = '__all__'
+    fields = ['titlu','completat']
     success_url = reverse_lazy('sarcini')
 
 class StergeSarcina(LoginRequiredMixin, DeleteView):
