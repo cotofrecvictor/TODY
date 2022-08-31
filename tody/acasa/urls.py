6a8577a882656d0bc6bf0419ee0ff17eb0216c8d
Cotofrec <cotofrecvictor@gmail.com>
2022-08-31 13:19:59 +0300

    add view and edit task views

--- tody/acasa/urls.py
+++ tody/acasa/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import CreazaSarcina, ListaSarcini
+from .views import CreazaSarcina, DetaliiSarcina, ListaSarcini,EditeazaSarcina
 
 urlpatterns = [
     path('', ListaSarcini.as_view(), name = 'sarcini'),
     path('creaza-sarcina/', CreazaSarcina.as_view(), name = 'creaza-sarcina'),
+    path('sarcina/<int:pk>/', DetaliiSarcina.as_view(), name = 'sarcina'),
+    path('editeaza-sarcina/<int:pk>/', EditeazaSarcina.as_view(), name = 'editeaza-sarcina'),
 ]
--- tody/acasa/views.py
+++ tody/acasa/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Sarcini
 
@@ -12,6 +13,16 @@ class ListaSarcini(ListView):
     context_object_name = 'sarcini'
 
 class CreazaSarcina(CreateView):
+    model = Sarcini
+    fields = '__all__'
+    success_url = reverse_lazy('sarcini')
+
+class DetaliiSarcina(DetailView):
+    model = Sarcini
+    context_object_name = 'sarcina'
+    template_name = 'acasa/sarcina.html'
+
+class EditeazaSarcina(UpdateView):
     model = Sarcini
     fields = '__all__'
     success_url = reverse_lazy('sarcini')
